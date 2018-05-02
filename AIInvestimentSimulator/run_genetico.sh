5dab3aa077fe6bd06f2b3757db8a8242510082e2
Luis Fernando Rocha de Sousa Alves <luis.alves@db1.com.br>
2018-05-02 17:55:31 -0300

    genetico e tentativa arquivo

--- AIInvestimentSimulator/run_genetico.sh
+++ AIInvestimentSimulator/run_genetico.sh
@@ -7,16 +7,15 @@
 #declare -a MONEY=("100000")
 #declare -a INSTANCES=("2013-2014,2015" "2014-2015,2016" "2015-2016,2017")
 
-declare -a PARAM_MIN_TO_BUY=("0.03")
-declare -a PARAM_MIN_TO_SELL=("0.15")
-declare -a PARAM_DISCARD_DIV=("2.5")
-declare -a PARAM_DAYS_TO_DISCARD=("5")
+declare -a PARAM_MIN_TO_BUY=("0" "0.5")
+declare -a PARAM_MIN_TO_SELL=("0" "0.5")
+declare -a PARAM_DISCARD_DIV=("0" "10")
+declare -a PARAM_DAYS_TO_DISCARD=("5" "30")
 declare -a PARAM_MIN_DAYS_TO_BEGIN=("0")
-declare -a PARAM_MAX_MONEY_TO_INVEST=("1")
+declare -a PARAM_MAX_MONEY_TO_INVEST=("1" "10")
 declare -a MONEY=("250000")
 #declare -a INSTANCES=("2011-2012,2013-2014-2015-2016-2017" "2012-2013,2014-2015-2016-2017" "2013-2014,2015-2016-2017" "2014-2015,2016-2017" "2015-2016,2017")
-#declare -a INSTANCES=("2015-2016,2017")
-declare -a INSTANCES=("2010-2011,2012")
+declare -a INSTANCES=("2015-2016,2017")
 
 
 
